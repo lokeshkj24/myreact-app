b12f55d8096ab95a4771d90568b780c4ea85fad1
Lokesh Kumar <36290710+lokeshkj24@users.noreply.github.com>
2023-05-19 01:40:50 +0530

    Updates

---
+++ precache-manifest.924a011d1bbecce81c74eeef873c5403.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3c746926b8bdb172cb0",
+    "url": "/lokeshkj24/myreact-app/static/css/main.0c82d6e1.chunk.css"
+  },
+  {
+    "revision": "d3c746926b8bdb172cb0",
+    "url": "/lokeshkj24/myreact-app/static/js/main.c2c40e33.chunk.js"
+  },
+  {
+    "revision": "8e282a5ab3d45b3dad47",
+    "url": "/lokeshkj24/myreact-app/static/js/runtime~main.7921a906.js"
+  },
+  {
+    "revision": "da49db07c0c6baf12ae4",
+    "url": "/lokeshkj24/myreact-app/static/js/2.3bfe5400.chunk.js"
+  },
+  {
+    "revision": "9e493117a3dfb5238748",
+    "url": "/lokeshkj24/myreact-app/static/js/3.f326d3da.chunk.js"
+  },
+  {
+    "revision": "46dbebdb759ef55c58eb2388a994abc1",
+    "url": "/lokeshkj24/myreact-app/index.html"
+  }
+];
